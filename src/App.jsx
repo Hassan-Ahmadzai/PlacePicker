7a24628bb5fa12b6a4d55f53cf02859dfedeae95
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-08-09 16:02:10 +0430

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,27 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
+import Places from './components/Places';
+import { AVAILABLE_PLACES } from './data';
+import logoImg from './assets/logo.png';
 import Modal from "./components/Modal";
 import DeleteConfirmation from "./components/DeleteConfirmation";
-import { AVAILABLE_PLACES } from "./data";
-import logoImg from './assets/logo.png';
-import Places from "./components/Places";
-import { sortPlacesByDistance } from "./loc";
-  
 
-const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-const storedPlaces = storedIds.map((id) => (
-    AVAILABLE_PLACES.find((place) => place.id === id)
-));
- 
 
 function App() {
-    const modal = useRef(); 
+    const modal = useRef();
     const selectedPlace = useRef();
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [availablePlaces, setAvailablePlaces] = useState([]);
     const [pickedPlaces, setPickedPlaces] = useState([]);
-
-
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition((position) => {
-            const sortedPlaces = sortPlacesByDistance(
-                AVAILABLE_PLACES,
-                position.coords.latitude,
-                position.coords.longitude,
-            );
-    
-            setAvailablePlaces(sortedPlaces);
-        });
-    }, [])
+    console.log(selectedPlace)
 
     function handleStartRemovePlace(id) {
-        // modal.current.open();
-        setModalIsOpen(true);
+        modal.current.open();
         selectedPlace.current = id;
     };
 
     function handleStopRemovePlace() {
-        // modal.current.close();
-        setModalIsOpen(false);
+        modal.current.close();
     };
 
-    function handleSelectPlace(id) {     
+    function handleSelectPlace(id) {
         setPickedPlaces((prevPickedPlaces) => {
             if (prevPickedPlaces.some((place) => place.id === id)) {
                 return prevPickedPlaces;
@@ -52,33 +29,18 @@ function App() {
             const place = AVAILABLE_PLACES.find((place) => place.id === id);
             return [place, ...prevPickedPlaces];
         });
-
-        const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-        if (storedIds.indexOf(id) === -1) {
-            localStorage.setItem(
-                'selectedPlaces',
-                JSON.stringify([id, ...storedIds])
-            );
-        };
     };
 
     function handleRemovePlace() {
         setPickedPlaces((prevPickedPlaces) => 
             prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
         );
-        // modal.current.close();
-        setModalIsOpen(false);
-
-        const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
-        localStorage.setItem(
-            'selectedPlaces',
-            JSON.stringify(storedIds.filter((id) => id !== selectedPlaces.current))
-        );
-    };  
+        modal.current.close();
+    };
 
     return (
-        <>      
-            <Modal open={modalIsOpen} onClose={handleStopRemovePlace}>
+        <>
+            <Modal ref={modal}>
                 <DeleteConfirmation 
                     onCancel={handleStopRemovePlace}
                     onConfirm={handleRemovePlace}
@@ -86,7 +48,7 @@ function App() {
             </Modal>
 
             <header>
-                <img src={logoImg} alt="Styled globe" />
+                <img src={logoImg} alt="Stylized globe" />
                 <h1>PlacePicker</h1>
                 <p>
                     Create your personal collection of places you would like to visit or you have visited.
@@ -95,16 +57,14 @@ function App() {
 
             <main>
                 <Places 
-                    title="I'd like to visit..."
+                    title="I'll like to visit ..."
                     fallbackText={"Select the places you would like to visit below."}
                     places={pickedPlaces}
                     onSelectPlace={handleStartRemovePlace}
                 />
- 
                 <Places 
                     title='Available Places'
-                    places={availablePlaces}
-                    fallbackText="Sorting places by distance..."
+                    places={AVAILABLE_PLACES}
                     onSelectPlace={handleSelectPlace}
                 />
             </main>
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,35 +1,26 @@
-import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+import { forwardRef, useImperativeHandle, useRef } from "react";
 import { createPortal } from "react-dom";
 
 
-const Modal = forwardRef(function Modal({ open, children, onClose }) {
+const Modal = forwardRef(function Modal({ children }, ref) {
     const dialog = useRef();
 
-    useEffect(() => {
-        if (open) {
-            dialog.current.showModal();
-        } else {
-            dialog.current.close();
+    useImperativeHandle(ref, () => {
+        return {
+            open: () => {
+                dialog.current.showModal();
+            },
+            close: () => {
+                dialog.current.close();
+            },
         };
-    }, [open]);
-
-    // useImperativeHandle(ref, () => {    
-    //     return {
-    //         open: () => {
-    //             dialog.current.showModal();
-    //         },
-    //         close: () => {
-    //             dialog.current.close();
-    //         },
-    //     };
-    // });
-
+    });
 
     return createPortal(
-        <dialog className="modal" ref={dialog} onClose={onClose}> 
-            {open ? children : null}
+        <dialog className="modal" ref={dialog}>
+            {children}
         </dialog>,
-        document.getElementById('modal-root')
+        document.getElementById('modal')
     );
 });
 
