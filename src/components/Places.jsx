057668c7be872b92718d422e8cc09510f5934a98
AhmadShakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-11-04 13:51:49 +0430

    Initial Commit

--- src/components/Places.jsx
+++ src/components/Places.jsx
@@ -4,7 +4,7 @@ export default function Places({ title, places, fallbackText, onSelectPlace }) {
         <section className="places-category">
             <h2>{title}</h2>
             {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-            {/* {places.length > 0 && (
+            {places.length > 0 && (
                 <ul className="places">
                     {places.map((place) => (
                         <li key={place.id} className="place-item">
@@ -15,7 +15,7 @@ export default function Places({ title, places, fallbackText, onSelectPlace }) {
                         </li>
                     ))}
                 </ul>
-            )} */}
+            )}
         </section>
     );
 };
